21c191351cb00b4f713b3449aa2e2e4e1e08c133
kadoglou <kadoglou.k@zdf.de>
2025-07-25 11:50:32 +0200

    fix: reference on docker_container nginx

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   name  = var.container_name
-  image = docker_image.nginx
+  image = docker_image.nginx.name
 
   ports {
     internal = 80
